30a4aa2a00e4323a4c2847cb3365539b06b0d0d8
AlexKapic <alexkapic@gmail.com>
2021-12-27 01:07:00 +0200

    fix propagination

--- client/src/components/tasks/tasks-container/task-card/task-card.tsx
+++ client/src/components/tasks/tasks-container/task-card/task-card.tsx
@@ -1,4 +1,5 @@
 import { ITask } from 'common/interfaces/task';
+import { SyntheticEvent } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import styles from './styles.module.scss';
 
@@ -13,6 +14,15 @@ export const TaskCard: React.FC<Props> = ({
   editHandler,
   deleteHandler,
 }) => {
+  const editTask = (e: SyntheticEvent, id: string): void => {
+    e?.stopPropagation();
+    editHandler(id);
+  };
+  const deleteTask = (e: SyntheticEvent, id: string): void => {
+    e?.stopPropagation();
+    deleteHandler(id);
+  };
+
   return (
     <Card className={styles.card} bg={task.isDone ? 'success' : 'secondary'}>
       <Card.Header className={styles.card_header}>
@@ -21,13 +31,13 @@ export const TaskCard: React.FC<Props> = ({
           <Button
             className="me-5"
             variant={task.isDone ? 'success' : 'secondary'}
-            onClick={(): void => editHandler(task.id)}
+            onClick={(e): void => editTask(e, task.id)}
           >
             Edit <i className="bi bi-pencil"></i>
           </Button>
           <Button
             variant={task.isDone ? 'success' : 'secondary'}
-            onClick={(): void => deleteHandler(task.id)}
+            onClick={(e): void => deleteTask(e, task.id)}
           >
             Delete <i className="bi bi-trash"></i>
           </Button>
